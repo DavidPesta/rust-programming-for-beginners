007e6105656061132734f0304c9d5a5528473694
David Pesta <davidpesta@gmail.com>
2022-01-25 22:47:08 -0600

    Completed second exercise.

--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -8,4 +8,14 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn sum(a: i128, b: i128) -> i128 {
+	a + b
+}
+
+fn display(a: i128) {
+	println!("{:?}", a);
+}
+
+fn main() {
+	display(sum(2938432984232444444444444422211133355, 98324823598322333333333333322277722244));
+}
